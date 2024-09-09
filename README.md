06ba358919b3884228d74c138378d086e4a7bf31
rinsanga <raltelalrinsanga@mediamonks.com>
2024-09-09 06:14:03 +0000

    number 1 done

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-will write to do later
+TODO:
+
+[x] write a script to read and print out worflow.json
+[] make a script that will queue prompt the worflow.json
+[] write a argeparse to read the worflow.json and print out the exposed params
+[] make argeparse  script to both print and queue the prompt
+[] add option for queing
+
--- make_flow.py
+++ make_flow.py
@@ -1,8 +1,19 @@
-"""
-    TOD:
-        read json script
-        make a .py file to run it
-        print out all the exposed Parameters
+import json
+
+with open("example_api.json", "r") as file:
+    data = json.load(file)
+
+
+for node_id, node in data.items():
+    if "exposed" in node["_meta"]["title"]:
+        print(f"Exposed Nodes : '{node['_meta']['title']}':")
+        print(node["inputs"])
+
 
 """
-
+print("example processing it")
+for node_id, node in data.items():
+    if "exposed" in node["_meta"]["title"]:
+        node["inputs"]["text"] = "changedinputs"
+        print(node["inputs"])
+    """
