f708e8672fc45c4aa5237d7d0e86d9fbb2d40791
rinsanga <raltelalrinsanga@mediamonks.com>
2024-09-09 07:14:32 +0000

    no 2 done

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 TODO:
 
 [x] write a script to read and print out worflow.json
-[] make a script that will queue prompt the worflow.json
+[x] make a script that will queue prompt the worflow.json
 [] write a argeparse to read the worflow.json and print out the exposed params
 [] make argeparse  script to both print and queue the prompt
 [] add option for queing
--- make_flow.py
+++ make_flow.py
@@ -1,6 +1,17 @@
+import argparse
+import requests
+import urllib.parse
+import urllib.request
+import uuid
+import websocket
+import random
 import json
 
-with open("example_api.json", "r") as file:
+parser = argparse.ArgumentParser(description="takes the path for the workflow")
+parser.add_argument("-q", "--queue", type=str, help="The path of the workflow")
+args = parser.parse_args()
+
+with open(args.queue, "r") as file:
     data = json.load(file)
 
 
@@ -17,3 +28,72 @@ for node_id, node in data.items():
         node["inputs"]["text"] = "changedinputs"
         print(node["inputs"])
     """
+
+
+server_address = "127.0.0.1:8188"
+client_id = str(uuid.uuid4())
+
+
+def queue_prompt(prompt):
+    p = {"prompt": prompt, "client_id": client_id}
+    data = json.dumps(p).encode("utf-8")
+    req = urllib.request.Request(
+        "http://{}/prompt".format(server_address), data=data)
+    return json.loads(urllib.request.urlopen(req).read())
+
+
+def get_image(filename, subfolder, folder_type):
+    data = {"filename": filename, "subfolder": subfolder, "type": folder_type}
+    url_values = urllib.parse.urlencode(data)
+    with urllib.request.urlopen(
+        "http://{}/view?{}".format(server_address, url_values)
+    ) as response:
+        return response.read()
+
+
+def get_history(prompt_id):
+    with urllib.request.urlopen(
+        "http://{}/history/{}".format(server_address, prompt_id)
+    ) as response:
+        return json.loads(response.read())
+
+
+def get_images(ws, prompt):
+    prompt_id = queue_prompt(prompt)["prompt_id"]
+    output_images = {}
+    while True:
+        out = ws.recv()
+        if isinstance(out, str):
+            message = json.loads(out)
+            if message["type"] == "executing":
+                data = message["data"]
+                if data["node"] is None and data["prompt_id"] == prompt_id:
+                    break  # Execution is done
+        else:
+            continue  # previews are binary data
+
+    history = get_history(prompt_id)[prompt_id]
+    for o in history["outputs"]:
+        for node_id in history["outputs"]:
+            node_output = history["outputs"][node_id]
+            if "images" in node_output:
+                images_output = []
+                for image in node_output["images"]:
+                    image_data = get_image(
+                        image["filename"], image["subfolder"], image["type"]
+                    )
+                    images_output.append(image_data)
+            output_images[node_id] = images_output
+
+    return output_images
+
+
+# load workflow from file
+
+workflow = data
+
+ws = websocket.WebSocket()
+ws.connect("ws://{}/ws?clientId={}".format(server_address, client_id))
+
+images = get_images(ws, workflow)
+print("dont queue ing")
